feb653126cebeb6ced31a6acf7f0a3214c2f24d0
Quintin Xu <QuintinHsu@gmail.com>
2018-07-13 22:42:43 +0800

    增加cookies更新机制

--- core/util.py
+++ core/util.py
@@ -3,12 +3,13 @@
 # @Author: Quintin Xu
 # @Date:   2018-07-11 19:04:11
 # @Last Modified by:   Quintin Xu
-# @Last Modified time: 2018-07-11 19:25:12
+# @Last Modified time: 2018-07-12 21:36:02
 # @E-mail: QuintinHsu@gmail.com
 # @Description: 
-
+import os
 from datetime import datetime, timedelta, timezone
 import time
+import random
 
 def localtime():
     """
@@ -30,3 +31,34 @@ def t_2_localtime(t):
     cn_dt = utc_dt.astimezone(timezone(timedelta(hours=8)))
     ts = time.mktime(cn_dt.timetuple())
     return ts
+
+def get_fake_user(ip_file, ua_file):
+    """
+    生成proxy-ua-cookies对
+    :param ip_file:     存储ip的文件地址
+    :param ua_file:     存储ua的文件地址
+    :return: dict   {'counter': 1, 'fu': [{'proxy': {'http': 'http://1.1.1.1:61000'}, 'ua': ua, 'cookies': None, 'counter': 0}]}
+    """
+    fake_user = {}
+    fake_user['counter'] = 0
+    fake_user['fu'] = list()
+    ips = list()
+    uas = list()
+
+    with open(ip_file, mode='r') as f:
+        ips = f.readlines()
+        ips = [ip.replace("\n", '') for ip in ips]
+
+    with open(ua_file, mode='r') as f:
+        uas = f.readlines()
+        uas = [ua.replace("\n", '') for ua in uas]
+
+    for ip in ips:
+        for i in range(80):
+            if i % 10 == 0:
+                ua = random.choice(uas)
+            fu = {'proxy': {'http': 'http://%s:61000' % ip}, 'ua': ua, 'cookies': None, 'counter': 0}
+            fake_user['fu'].append(fu)
+    return fake_user
+
+
