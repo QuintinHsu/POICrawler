c31175842b4c4f5e647aac86ca98287718480ee6
Quintin Xu <QuintinHsu@gmail.com>
2018-07-20 11:31:44 +0800

    修改百度抓取关键词路径

--- baidu_poi.py
+++ baidu_poi.py
@@ -3,7 +3,7 @@
 # @Author: Quintin Xu
 # @Date:   2018-07-10 19:28:40
 # @Last Modified by:   Quintin Xu
-# @Last Modified time: 2018-07-19 10:04:32
+# @Last Modified time: 2018-07-20 11:30:05
 # @E-mail: QuintinHsu@gmail.com
 # @Description: 抓取百度POI，百度POI每次请求最多返回50条数据，page_num从0开始，需要设置城市边界，不要设置城市代码，否则在边界区域会搜索不到数据
 
@@ -320,7 +320,7 @@ def init_queue(init_bound, redis_key=REDIS_KEY_POIREQUEST_BD):
     url = 'http://map.baidu.com'
 
     baidu_poi_wd = list()
-    with open('./data/classification_poi', 'r') as f:
+    with open('./data/baidu_keywords', 'r') as f:
         baidu_poi_wd = f.readlines()
         baidu_poi_wd = [wd.replace("\n", '') for wd in baidu_poi_wd]
 
